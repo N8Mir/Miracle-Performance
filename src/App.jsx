f9b1a1627b8d170a7ce3df59c86bfa046b7b6ae3
N8Mir <nate@mpcle.com>
2026-02-25 15:18:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SiteHeader from "./components/SiteHeader";
-import SiteFooter from "./components/SiteFooter";
-import MobileStickyCTA from "./components/MobileStickyCTA";
-import ScrollManager from "./components/ScrollManager";
 
-import HomePage from "./pages/HomePage";
-import PricingPage from "./pages/PricingPage";
-import FreeWeekPage from "./pages/FreeWeekPage";
+import SiteHeader from "./components/SiteHeader.jsx";
+import SiteFooter from "./components/SiteFooter.jsx";
+import MobileStickyCTA from "./components/MobileStickyCTA.jsx";
+import ScrollManager from "./components/ScrollManager.jsx";
+
+import HomePage from "./pages/HomePage.jsx";
+import PricingPage from "./pages/PricingPage.jsx";
+import FreeWeekPage from "./pages/FreeWeekPage.jsx";
 
 export default function App() {
   return (
@@ -15,13 +16,11 @@ export default function App() {
       <ScrollManager />
       <div className="min-h-screen bg-white text-gray-900 pb-20 md:pb-0">
         <SiteHeader />
-
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/pricing" element={<PricingPage />} />
           <Route path="/free-week" element={<FreeWeekPage />} />
         </Routes>
-
         <SiteFooter />
         <MobileStickyCTA />
       </div>
